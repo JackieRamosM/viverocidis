8734370c37aa98b80983fe27a94aafdf36c238a7
Jacqueline Ramos <jackie.ljrm@gmail.com>
2015-01-06 09:45:59 -0500

    ventilacion y manipulacion de foco en tiempo real

--- Vivero/app/src/main/java/espol/cidis/vivero/vivero/Controles.java
+++ Vivero/app/src/main/java/espol/cidis/vivero/vivero/Controles.java
@@ -2,21 +2,124 @@ package espol.cidis.vivero.vivero;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.Switch;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by soporte on 12/12/14.
  */
 public class Controles extends Activity {
+    Switch foco;
+    Spinner motor;
+    String modo="On";
+    String umodo;
+    String control;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.controles);;
+        setContentView(R.layout.controles);
+        foco = (Switch) findViewById(R.id.foco);
+
+
+        List<String> spinnerArray =  new ArrayList<String>();
+        spinnerArray.add("Apagado");
+        spinnerArray.add("Derecha");
+        spinnerArray.add("Izquierda");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        motor = (Spinner) findViewById(R.id.vent);
+        motor.setAdapter(adapter);
+
+
+        foco.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                if(isChecked){
+
+                    StrictMode.ThreadPolicy policy = new
+                            StrictMode.ThreadPolicy.Builder()
+                            .permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+                    control="Turn";
+                    modo="On";
+                    new Connection().execute();
+
+
+                }else{
+                    control="Turn";
+                    modo="On";
+                    new Connection().execute();
+
+                }
+
+            }
+        });
+
+        motor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int myPosition, long myID) {
+
+                String selected = motor.getSelectedItem().toString();
+                if (selected.equals("Derecha")) {
+                    control="Motor";
+                    modo="Right";
+                    umodo=modo;
+                    new Connection().execute();
+                }
+                if (selected.equals("Izquierda")) {
+                    control="Motor";
+                    modo="Left";
+                    umodo=modo;
+                    new Connection().execute();
+                }
+                if (selected.equals("Apagado")) {
+                    control="Motor";
+                    modo=umodo;
+                    new Connection().execute();
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+
+
     }
 
 
@@ -41,5 +144,33 @@ public class Controles extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+          private class Connection extends AsyncTask {
+
+            @Override
+            protected Object doInBackground(Object... arg0) {
+                connect();
+                return null;
+            }
+
+        }
+
+        private void connect() {
+            String responseStr="";
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://200.126.19.117:8080");
+            try {
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+                nameValuePairs.add(new BasicNameValuePair(control, modo));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+            } catch (ClientProtocolException e) {
+                Log.d("HTTPCLIENT", e.getLocalizedMessage());
+            } catch (IOException e) {
+                Log.d("HTTPCLIENT", e.getLocalizedMessage());
+            }
+        }
 
 }
